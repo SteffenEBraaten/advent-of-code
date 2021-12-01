059e28c08bc36b0a4e69898b923e3e7139baf2ac
SteffenEBraaten <steffen.e.braaten@gmail.com>
2021-12-01 18:03:46 +0100

    Completed 1b

--- day1/countLargerThanPrevious.py
+++ day1/countLargerThanPrevious.py
@@ -22,9 +22,22 @@ def count_larger_than_previous_measurement(listOfNumbers):
             number_of_larger_than_previous = number_of_larger_than_previous + 1
     return number_of_larger_than_previous
 
+def create_three_measurement_sliding_window(listOfNumbers):
+    numberList = []
+    for index, number in enumerate(listOfNumbers):
+        if (index + 2) < len(listOfNumbers):
+            numberList.append(number + listOfNumbers[index + 1] + listOfNumbers[index + 2])
+    return numberList
+
+
 def main():
     file = 'input.txt'
-    sum = count_larger_than_previous_measurement(__read_file_of_numbers(file))
+    listOfNumbers = __read_file_of_numbers(file)
+    sum = count_larger_than_previous_measurement(listOfNumbers)
     print(f'There are {sum} measueres that are larger than its previous measure')
+    
+    three_measurement_sliding_window_list = create_three_measurement_sliding_window(listOfNumbers)
+    three_measurement_sliding_sum = count_larger_than_previous_measurement(three_measurement_sliding_window_list)
+    print(f'There are {three_measurement_sliding_sum} three-measueres that are larger than its previous measure')
 
 main()
